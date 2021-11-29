81dc2a3a4781b5448ffb8c1f03e8e53d96f00afc
shiris <shuroo83@gmail.com>
2021-11-30 01:57:26 +0200

    fixed the boolean method, added refresh button, and more..

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
  
 import React, { Component,useState,useEffect } from 'react';
+import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Alert from 'react-bootstrap/Alert';
 import './style.css';
@@ -7,45 +8,71 @@ import DisplayBoard from '../components/DisplayBoard';
 function HomePage(){
 
     
-    const [board, setBoard] = useState(["   ","   ","  "," ","  ","  ","  ","  ","  "]); 
+    const [board, setBoard] = useState(["","","","","","","","",""]); 
     const [turn,setTurn] =  useState("X"); 
     const [won,setWon] =  useState(null); 
 
-    function checkRow(box1,box2,box3){
-        return ( box1.trim().length > 0 && box1.trim()===box2.trim() ===box3.trim() );
+    function checkRow(index1,index2,index3){
+        var box1 = board[index1];
+        var box2 = board[index2];
+        var box3 = board[index3];
+        return ( box1 !=="" && box1===box2 && box2===box3 );
     }
 
     function checkIsWon(){ 
-        var isWon = [ checkRow(board[0],board[3],board[6]) || 
-        // checkRow(board[0],board[1],board[2]) ||
-        // checkRow(board[3],board[4],board[5]) ||
-        // checkRow(board[6],board[7],board[8]) || 
-        // checkRow(board[1],board[4],board[7]) ||
-        // checkRow(board[2],board[5],board[8]) ||
-        checkRow(board[0],board[4],board[8]) 
-           
-    ]
+             var checkCol1 = checkRow(0,3,6);
+             var checkRow1 = checkRow(0,1,2);
+             var checkRow2 = checkRow(3,4,5);
+             var checkRow3 = checkRow(6,7,8);
+             var checkDiag1 = checkRow(0,4,8)
+             var checkDiag2 = checkRow(2,4,6)
+             var checkCol2 = checkRow(1,4,7)
+             var checkCol3 = checkRow(2,5,8)
 
-                if(isWon){setWon(turn)};
+             let isWon = (true===checkCol3) || (true===checkCol2) ||
+             (true===checkCol1) || (true===checkRow1) ||
+             (true===checkRow2) || (true===checkRow3) ||
+             (true===checkDiag1) || (true===checkDiag2);
+    
+
+      // todo: how to use this properly???
+        if(isWon)
+            {
+                setWon(turn);
+            };
     }
     
     function onClickCell(index){
         let cpyBoard = [ ... board];
         let nextTurn = (turn==="X")? "O" : "X";
-        cpyBoard[index] = nextTurn;
-        setTurn(nextTurn);
-        setBoard(cpyBoard);
+        if(cpyBoard[index] === ''){
+            cpyBoard[index] = nextTurn;
+            setTurn(nextTurn);
+            setBoard(cpyBoard);
+        }
     }
+
+    function refreshPage() {
+        window.location.reload(false);
+      }
    
     useEffect(()=>{ checkIsWon()},[board]);
 
-    const wonAlert = (won?<Alert className="info alertBox">You Won!</Alert> : null);
+    const wonAlert = ((won != null)?<Alert className="info alertBox">{turn} Won!</Alert> : null);
     const boardElement = (won?null : <DisplayBoard board={board} onClickCell={onClickCell} /> );
-   return <Container>{wonAlert}
-   
+   return <div><Container>{wonAlert}
+  
    <div>{boardElement} </div>
-   </Container>
 
+
+   </Container>
+   <Container>
+<Button onClick={refreshPage} variant="secondary">Start Over!
+       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bootstrap-reboot" viewBox="0 0 16 16">
+  <path d="M1.161 8a6.84 6.84 0 1 0 6.842-6.84.58.58 0 1 1 0-1.16 8 8 0 1 1-6.556 3.412l-.663-.577a.58.58 0 0 1 .227-.997l2.52-.69a.58.58 0 0 1 .728.633l-.332 2.592a.58.58 0 0 1-.956.364l-.643-.56A6.812 6.812 0 0 0 1.16 8z"/>
+  <path d="M6.641 11.671V8.843h1.57l1.498 2.828h1.314L9.377 8.665c.897-.3 1.427-1.106 1.427-2.1 0-1.37-.943-2.246-2.456-2.246H5.5v7.352h1.141zm0-3.75V5.277h1.57c.881 0 1.416.499 1.416 1.32 0 .84-.504 1.324-1.386 1.324h-1.6z"/>
+</svg></Button>{' '}</Container>
+</div>
 }
 
 export default HomePage;
